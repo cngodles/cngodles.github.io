2aac368cd16a3294af3e5beca6d9490c3780de54
Clint <cngodles@gmail.com>
2018-04-27 15:36:23 -0400

    Add: Planets

---
+++ planets/js/planets.events.js
@@ -0,0 +1,20 @@
+$(document)
+.ready(function(){
+  planet.init();
+})
+.on("keyup change", "#sunsize", function(){
+  planet.focalsize = $(this).val();
+  planet.setMeasurements();
+})
+.on("click", ".action_choosebase", function(e){
+  e.preventDefault();
+  $(".basechoices").toggleClass("open");
+})
+.on("click", ".basechoices a.choice", function(){
+  $("#input_step2").show();
+  planet.targetbody = $(this).attr("rel");
+  $(".basechoices").removeClass("open");
+  $("#sunsize").trigger("keyup");
+  $("h2.choicename").text("Enter size for "+planet.targetbody);
+})
+;
